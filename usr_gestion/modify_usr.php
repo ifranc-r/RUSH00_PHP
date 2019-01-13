243933ceccb23f72c890370c760637fd106132a2
Inti FRANC-REGIS <ifranc-r@e3r11p19.42.fr>
2019-01-13 16:04:14 +0100

    create,dele create.html

--- usr_gestion/modify_usr.php
+++ usr_gestion/modify_usr.php
@@ -6,7 +6,7 @@ function ft_is_null($var){
 }
 
 
-function create_usr(){
+function modify_usr(){
 	// connection to data
 	$conn = connecte2data();
 	if (!$conn) {
@@ -48,5 +48,5 @@ function create_usr(){
 	mysqli_close($conn);
 }
 
-create_usr();
+modify_usr();
 ?>
