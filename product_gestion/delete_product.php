644f874c6f0ff1f5bf2c7ef00ecc110d7fa6d820
Inti FRANC-REGIS <ifranc-r@e3r11p19.42.fr>
2019-01-13 20:34:12 +0100

    install

--- product_gestion/delete_product.php
+++ product_gestion/delete_product.php
@@ -1,3 +1,23 @@
 <?php
+require_once('../mysql_db/connect2db.php');
+
+function delete_product($id){
+	// connection to data
+	$conn = connecte2data();
+	if (!$conn)
+		exit ("Connection failed: " . mysqli_connect_error());
+	$check_product = "SELECT * FROM products WHERE id='$id'";
+
+ 	if (mysqli_query($conn, $check_product)){
+ 		$sql_del_product = "DELETE FROM products
+							WHERE id='$id'";
+		if (mysqli_query($conn, $sql_del_product)){
+			echo ("Product is deleted\n");
+		}
+	}
+	else
+		echo ("ERROR\n");
+	mysqli_close($conn);
+}
 
 ?>
