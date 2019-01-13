ca61ff89914de52db566e36903a4892119cca4b5
Inti FRANC-REGIS <ifranc-r@e3r11p19.42.fr>
2019-01-13 15:45:24 +0100

    creat and del user

--- mysql_db/connect2db.php
+++ mysql_db/connect2db.php
@@ -1,9 +1,10 @@
 <?php
 
-function connecte2data($db){
+function connecte2data(){
 	$username_db = "inti";
 	$password_db = "rootme";
 	$link_mysql = "localhost:3306";
+	$db = "db_rush00";
 
 	$mysqli = mysqli_connect($link_mysql, $username_db, $password_db, $db);
 	if (mysqli_connect_errno($mysqli))
--- usr_gestion/delete_usr.php
+++ usr_gestion/delete_usr.php
@@ -6,27 +6,27 @@ function ft_is_null($var){
 }
 
 
-function main(){
+function delete_user(){
 	// connection to data
 	$conn = connecte2data();
 	if (!$conn) {
 		exit ("Connection failed: " . mysqli_connect_error());
 	}
 	// check Post correct
-	if ($_POST["submit"] == "Creer un compte" && ft_is_null($_POST["login"]) && ft_is_null($_POST["password"])){
+	if ($_POST["submit"] == "OK" && ft_is_null($_POST["login"]) && ft_is_null($_POST["password"])){
 		// create var of user and pass
 		list($login, $password) = array($_POST["login"], $_POST["password"]);
 		// SQL check line for check
 		$check_pass = "SELECT * FROM users WHERE login='$login' AND password=PASSWORD('$password')";
 		// check if user exist
 		if ($result = mysqli_query($conn, $check_pass)){
-			if (mysqli_num_rows($result) == 0){
+			if (mysqli_num_rows($result) == 1){
 				// create user on SQL line
-				$insert_user = "INSERT INTO users (login, password, admin)
-								VALUES ('$login', PASSWORD('$password'), 0)";
+				$insert_user = "DELETE FROM table_name
+								WHERE [condition];";
 				// apply SQL line on database
 				if (mysqli_query($conn, $insert_user)){
-					echo ("User is create\n");
+					echo ("User is deleted\n");
 					// send user to home page
 					// header("Location: index.html")
 				}
@@ -34,7 +34,7 @@ function main(){
 					echo ("ERROR\n");
 			}
 			else
-				echo ("User exist already\n");
+				echo ("User doesn't exist\n");
 		}
 		// free sql result
     	mysqli_free_result($result);
@@ -45,5 +45,5 @@ function main(){
 	mysqli_close($conn);
 }
 
-main();
+delete_user();
 ?>
